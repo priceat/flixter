9487d5f84e0083f7b13f00d3214fa9e2b25e5421
Tate Price <priceat@gmail.com>
2015-03-04 19:39:01 +0000

    Lesson Authentication

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
+    course = current_lesson.section.course
+    if current_user.enrolled_in?(course)
+     render current_lesson
+    else
+      flash[:alert] = "You Must Be Enrolled to See Lessons"
+      redirect_to course_path(course)
+    end
   end
 
   private
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
